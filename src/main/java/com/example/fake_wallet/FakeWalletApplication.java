d38f8d32461f52bb793480decfac71557a3ccba7
dhairya1313 <dhairyasharma0113@gmail.com>
2025-11-15 21:53:35 +0530

    Initial commit

---
+++ src/main/java/com/example/fake_wallet/FakeWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.fake_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fake_wallet/FakeWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fake_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
